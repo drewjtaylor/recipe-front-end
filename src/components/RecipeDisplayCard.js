e56595243a2b6dec9ee784205e821c40c39fc100
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-10-04 15:21:33 -0400

    Installed interweave/markup to handle http/text

--- src/components/RecipeDisplayCard.js
+++ src/components/RecipeDisplayCard.js
@@ -1,26 +1,37 @@
 import Button from "react-bootstrap/Button";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import Card from "react-bootstrap/Card";
+import { Markup } from "interweave";
 
 // Accepts a single recipe object and displays information on a card
 
 const RecipeDisplayCard = ({ recipeObject }) => {
-  if (recipeObject) {
+    if (recipeObject === {}) {return null} else
+  
+  
+    if (recipeObject) {
     const recipe = recipeObject.recipes[0];
-    return (
-      <Card>
-        {/* <Card.img variant='top' src={recipe.image} /> */}
-        <Card.Img
-          variant="top"
-          src={"https://spoonacular.com/recipeImages/655525-556x370.jpg"}
-        />
-        <Card.Body>
-          <Card.Title>{recipe.title}</Card.Title>
-          <Card.Text dangerouslySetInnerHTML={{__html: recipe.summary}}/>
-          <Button as="a" href={recipe.sourceUrl} variant="primary">
-            Go Somewhere
-          </Button>
-        </Card.Body>
-      </Card>
+    
+    return ( <>
+        <Row className="justify-content-center">
+            <Col xs={8}>
+                <Card>
+                    <Card.Img
+                    variant="top"
+                    src={"https://spoonacular.com/recipeImages/655525-556x370.jpg"}
+                    />
+                    <Card.Body>
+                    <Card.Title>{recipe.title}</Card.Title>
+                    <Card.Text><Markup content={recipe.summary} /></Card.Text>
+                    <Button as="a" href={recipe.sourceUrl} variant="primary">
+                        Go Somewhere
+                    </Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+    </>
     );
   }
 };
