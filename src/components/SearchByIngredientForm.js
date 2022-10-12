c9c8ac0b7f21c6266d5005266d433d41226d340c
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-10-12 16:00:22 -0400

    Corrected some form text

--- src/components/SearchByIngredientForm.js
+++ src/components/SearchByIngredientForm.js
@@ -1,21 +1,25 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 const SearchByIngredientForm = () => {
-  return (
+    const [ingredient, setIngredient] = useState('');
+  
+  
+    return (
     <>
         <hr />
         <h2>SearchByIngredientForm</h2>
         <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Ingredient</Form.Label>
-            <Form.Control type="ingredient" placeholder="Ingredient" />
+            <Form.Control type="ingredient" placeholder="Type an ingredient here and hit submit" />
             <Form.Text className="text-muted">
             Enter a single ingredient here then hit "Submit" to find recipies that have that ingredient
             </Form.Text>
         </Form.Group>
-
-
         <Button variant="primary" type="submit">
             Submit
         </Button>
