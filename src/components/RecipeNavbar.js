e5ce64f3eb78ef5849cd020df8a70f70274e9b13
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-11-06 00:44:20 -0400

    Form submitting values to console.log successfully

--- src/components/RecipeNavbar.js
+++ src/components/RecipeNavbar.js
@@ -8,41 +8,42 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const RecipeNavbar = () => {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
     const [show, setShow] = useState(false);
 
-    const modalRef = useRef();
-    modalRef.current = false;
-
-    const toggleModal = () => modalRef.current=!(modalRef.current)
+    const toggleModal = () => setShow(prev => !prev)
 
     const handleSubmit = (e) => {
-        toggleModal();
-        console.log(JSON.stringify(e, null, 2))
+        e.preventDefault();
+        console.log(emailRef.current.value);
+        console.log(passwordRef.current.value);
     }
 
   return (
     <Navbar bg="primary" variant="dark" expand="lg" sticky='top'>
       
-      <Modal show={modalRef.current} onHide={toggleModal}>
-        <Modal.Header closeButton>
-            <Modal.Title>Enter your email and password to sign in below</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email Address</Form.Label>
-                    <Form.Control className="mb-3" type="email" placeholder="Enter email here" />
-                </Form.Group>
-                <Form.Group controlId="formPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control className="mb-3" type="password" placeholder="Enter password here" />
-                </Form.Group>
-            </Form>
-        </Modal.Body>
-        <Modal.Footer>
-            <Button variant='primary' onClick={handleSubmit}>Log in </Button>
-            <Button variant='primary' onClick={toggleModal}>Sign up </Button>
-        </Modal.Footer>
+      <Modal show={show} onHide={toggleModal}>
+        <Form onSubmit={handleSubmit}>
+            <Modal.Header closeButton>
+                <Modal.Title>Enter your email and password to sign in below</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                    <Form.Group controlId="email">
+                        <Form.Label>Email Address</Form.Label>
+                        <Form.Control ref={emailRef} className="mb-3" type="email" placeholder="Enter email here"/>
+                    </Form.Group>
+                    <Form.Group controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control ref={passwordRef} className="mb-3" type="password" placeholder="Enter password here"/>
+                    </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant='primary' type="submit">Log in </Button>
+                <Button variant='primary' onClick={toggleModal}>Close (evetually should be "sign up") </Button>
+            </Modal.Footer>
+        </Form>
       </Modal>
       
       
