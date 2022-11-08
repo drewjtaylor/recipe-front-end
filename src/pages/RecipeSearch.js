45c77c7cde09c060c6c231312c76aa26cf5c79da
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-11-08 15:12:44 -0500

    expanded search by ingredient

--- src/pages/RecipeSearch.js
+++ src/pages/RecipeSearch.js
@@ -9,6 +9,9 @@ import Button from 'react-bootstrap/Button';
 const RecipeSearch = () => {
     const ingredient = useRef();
     const bannedIngredients = useRef();
+    const numberOfResults = useRef();
+    const typeOfDiet = useRef();
+    const typeOfIntolerances = useRef();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,12 +25,46 @@ const RecipeSearch = () => {
         <Form onSubmit={handleSubmit}>
             <Form.Group className='mb-2 col-6'>
                 <Form.Label>Required Ingredient</Form.Label>
-                <Form.Control ref={ingredient} type='text' />
+                <Form.Control ref={ingredient} type='text' placeholder='Enter one ingredient you MUST have in your recipe' />
             </Form.Group>
             <Form.Group className='mb-2 col-6'>
                 <Form.Label>Banned Ingredient</Form.Label>
-                <Form.Control ref={bannedIngredients} type='text' />
+                <Form.Control ref={bannedIngredients} type='text' placeholder='Enter one ingredient to ban'/>
             </Form.Group>
+            <Form.Group className='mb-2 col-6'>
+                <Form.Label>Number of results</Form.Label>
+                <Form.Control ref={numberOfResults} type='text' placeholder='Leave blank for 4 recipes'/>
+            </Form.Group>
+            <Form.Group className='mb-2 col-6'>
+                <Form.Label>Type of Diet (optional)</Form.Label>
+                <Form.Select ref={typeOfDiet} area-label="Choose a type of diet">
+                    <option>Select intolerances</option>
+                    <option value='Gluten Free'>Gluten Free</option>
+                    <option value='Ketogenic'>Ketogenic</option>
+                    <option value='Vegetarian'>Vegetarian</option>
+                    <option value='Lacto-Vegetarian'>Lacto-Vegetarian</option>
+                    <option value='Ovo-Vegetarian'>Ovo-Vegetarian</option>
+                    <option value='Vegan'>Vegan</option>
+                    <option value='Pescetarian'>Pescetarian</option>
+                    <option value='Paleo'>Paleo</option>
+                    <option value='Primal'>Primal</option>
+                    <option value='Whole30'>Whole30</option>
+                </Form.Select>
+            </Form.Group>
+                <Form.Label>Intolerances (switch "on" to ban from results)</Form.Label>
+
+                {['Dairy', 'Egg', 'Gluten', 'Grain', 'Peanut',
+                  'Seafood', 'Sesame', 'Shellfish', 'Soy',
+                  'Sulfite', 'TreeNut', 'Wheat'].map(intolerance => {
+                    return <div key={intolerance} className='mb-1'>
+                        <Form.Check
+                            type='switch'
+                            id={intolerance}
+                            label={intolerance}
+                        />
+                    </div>
+                })}
+
             <Button type='submit'>Submit</Button>
         </Form>
     </Container>
